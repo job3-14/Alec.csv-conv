7d899c04ff7b63d751f3a7b6b3968e5339310dc1
job314 <job314@SMBA2.local>
2025-02-18 16:53:27 +0900

    発送実験と修正

--- program/add_yamato.py
+++ program/add_yamato.py
@@ -1,30 +1,33 @@
+#!/usr/bin/env python
 """
 ヤマト運輸CSVを補完するプログラム
 
 orders.csvに情報を補完し、csvを更新後、元ファイルを削除
 """
-
 import csv
 import configparser
 import os
+import sys
+import webbrowser
 
 def main():
     # config.ini読み込み
     config_ini = configparser.ConfigParser()
-    config_ini.read('~/.alec/config.ini', encoding='utf-8')
+    config_path = os.path.expanduser('~/.alec/config.ini')
+    config_ini.read(config_path, encoding='utf-8')
 
     # メルカリCSV(B2仕様)読み込み
+    orders_csv_path = os.path.expanduser('~/Downloads/orders.csv')
     try:
-        with open('~/Desktop/orders.csv') as f:
+        with open(orders_csv_path) as f:
             reader = csv.reader(f)
             csv_data = [row for row in reader]
     except Exception as e:
         print("エラー：メルカリCSV ファイルを開くことができません.")
         sys.exit()
 
-
-
-    for i in range(1,len(csv_data)):
+    # CSVデータの補完処理
+    for i in range(1, len(csv_data)):
         data = csv_data[i]
         product_code = data[29]
         data[1] = config_ini[product_code]['package_type']
@@ -37,39 +40,22 @@ def main():
         data[41] = config_ini['DEFAULT']['freight_control_number']
         csv_data[i] = data
 
-
-    with open('~/Desktop/orders_add.csv', 'w') as f:
+    # 補完後のCSVを書き出す
+    orders_add_csv_path = os.path.expanduser('~/Desktop/orders_add.csv')
+    with open(orders_add_csv_path, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(csv_data)
 
-    os.remove('~/Desktop/orders.csv')
-
+    # 元のCSVを削除
+    os.remove(orders_csv_path)
     print('orders.csv を削除しました')
     print('orders_add.csv を保存しました.')
     print('ヤマトで伝票発行後, 追跡データをダウンロードして一括発送を行なってください.')
     print('ヤマト運輸にアップロード後エンターを押してください')
+    yamato_url = 'https://bmypage.kuronekoyamato.co.jp/bmypage/'
+    webbrowser.open(yamato_url, new=2, autoraise=True)
     input()
-    os.remove('~/Desktop/orders_add.csv')
-
-
-
-
-'''
-不足分
-1 送り状種類
-2 クール区分
-19 ご依頼主電話番号
-21 ご依頼主郵便番号
-22 ご依頼主住所
-27 品名１
-39 請求先顧客コード
-41 運賃管理番号
-
-
-
-読み込み
-29 品名２ == MIX
-'''
+    os.remove(orders_add_csv_path)
 
 if __name__ == "__main__":
     try:
--- program/shipping.py
+++ program/shipping.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 ヤマト運輸CSVを補完するプログラム
 
@@ -5,8 +6,9 @@ orders.csvに情報を補完し、csvを更新後、元ファイルを削除
 """
 
 import csv
-#import configparser
 import os, sys
+import webbrowser
+import glob
 
 
 def search_tracking_id(data, key):
@@ -21,19 +23,54 @@ def search_tracking_id(data, key):
         # 行が十分な要素数を持っているかチェック
         if len(row) > 3 and row[0] == key:
             row_id = str(row[3])
-            formatted_id = 'ヤマト運輸: '+row_id[:4] + '-' + row_id[4:8] + '-' + row_id[8:]
+            formatted_id = 'ヤマト運輸: ' + row_id[:4] + '-' + row_id[4:8] + '-' + row_id[8:]
             return formatted_id
     print("発送IDが見つかりません. ヤマト運輸側の出力年月日が正しいか確認してください.")
     return None
 
 
-# config.ini読み込み
-#config_ini = configparser.ConfigParser()
-#config_ini.read('config.ini', encoding='utf-8')
+def find_yamato_csv_file():
+    '''
+    *_発送済データ.csvを検索し、返す関数
+    複数見つかった場合はプログラムを終了する.
+    '''
+    # ホームディレクトリ内の「Downlods」フォルダを指定
+    search_dir = os.path.expanduser("~/Downloads")
+    # ワイルドカード * を使用してパターンを指定
+    pattern = os.path.join(search_dir, "*_発行済データ.csv")
+    
+    # パターンに一致するファイルを検索
+    files = glob.glob(pattern)
+    
+    # 複数ファイルが見つかった場合
+    if len(files) > 1:
+        print("複数の'_発行済データ.csv'見つかりました")
+        print("全て削除してください")
+        sys.exit(1)
+    # 該当ファイルが見つからなかった場合（必要に応じて処理を変更）
+    elif len(files) == 0:
+        print("ファイルが見つかりません")
+        sys.exit(1)
+    
+    # 単一ファイルが見つかった場合、そのパスを返す
+    return files[0]
+
+
 def main():
+    print('ヤマトにログイン後、エンターキーを押してください.')
+    yamato_login = 'https://bmypage.kuronekoyamato.co.jp/bmypage/'
+    webbrowser.open(yamato_login, new=2, autoraise=True)
+    input()
+
+    print('一括発送CSVをダウンロードしてください.')
+    print('ダウンロード後エンターキーを押してください.')
+    mercari_url = 'https://mercari-shops.com/seller/shops/66LLW6ENCaaEex3Zs7sZde/orders/upload/'
+    webbrowser.open(mercari_url, new=2, autoraise=True)
+    input()
     # メルカリCSV(B2仕様)読み込み
+    orders_csv_path = os.path.expanduser('~/Downloads/orders.csv')
     try:
-        with open('~/Desktop/orders.csv') as f:
+        with open(orders_csv_path) as f:
             reader = csv.reader(f)
             csv_data_mercari = [row for row in reader]
     except Exception as e:
@@ -41,50 +78,49 @@ def main():
         sys.exit()
 
     # ヤマトCSV読み込み
-    print("ヤマト運輸 発行済データ(csv)をドラッグ&ドロップしてください.")
-    yamato_data_path = input()
+    print('ヤマト発送済みCSVをダウンロードしてください.')
+    print('ダウンロード後エンターキーを押してください.')
+    yamato_url = 'https://newb2web.kuronekoyamato.co.jp/issue_search.html'
+    webbrowser.open(yamato_url, new=2, autoraise=True)
+    input()
+    yamato_data_path = find_yamato_csv_file()
+    print(yamato_data_path)
     try:
-        with open(yamato_data_path) as f:
+        with open(yamato_data_path, encoding='cp932') as f:
             reader = csv.reader(f)
             csv_data_yamato = [row for row in reader]
     except Exception as e:
         print("エラー：ヤマト運輸 発行済データ(csv)ファイルを開くことができません.")
         sys.exit()
 
-
-    for i in range(1,len(csv_data_mercari)):
+    # メルカリCSVに追跡番号情報を追加
+    for i in range(1, len(csv_data_mercari)):
         data = csv_data_mercari[i]
-        id = data[0]
-        tracking = search_tracking_id(csv_data_yamato, id)
+        order_id = data[0]
+        tracking = search_tracking_id(csv_data_yamato, order_id)
+        if tracking is None:
+            print("追跡情報が存在しません")
+            os.remove(yamato_data_path)
+            sys.exit(1)
         data[14] = tracking
         csv_data_mercari[i] = data
 
-
-    with open('~/Desktop/orders_ship.csv', 'w') as f:
+    # 補完後のCSVを書き出す
+    orders_ship_csv_path = os.path.expanduser('~/Desktop/orders_ship.csv')
+    with open(orders_ship_csv_path, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(csv_data_mercari)
     
-    os.remove('~/Desktop/orders.csv')
+    # 元のCSVファイルとヤマトCSVファイルを削除
+    os.remove(orders_csv_path)
     os.remove(yamato_data_path)
     print('orders_ship.csv を保存しました.')
     print('メルカリで一括発送を行ってください')
     print('メルカリにアップロード後エンターを押してください')
     input()
-    os.remove('~/Desktop/orders_ship.csv')
+    os.remove(orders_ship_csv_path)
 
 
-'''
-
-
-print('orders.csv を削除しました')
-print('orders_add.csv を保存しました.')
-
-'''
-
-'''
-0 order_id
-14 shipping_tracking_info
-'''
 if __name__ == "__main__":
     try:
         main()
